ccaa72eef08910c6502300b44a52acc425158f4f
Daniil Alekseev <lashy.danya.work@gmail.com>
2025-01-10 20:40:50 +0300

    initialize ILI9488 LCD panel and configure SPI bus

--- firmware/main/app_main.c
+++ firmware/main/app_main.c
@@ -1,23 +1,115 @@
 #include <stdio.h>
 
-#include "esp_flash.h"
-#include "esp_system.h"
-#include "esp_chip_info.h"
+#include "esp_err.h"
+#include "esp_log.h"
+#include "driver/gpio.h"
+#include "driver/spi_master.h"
+#include "esp_lcd_panel_io.h"
+#include "esp_lcd_panel_ops.h"
+#include "esp_lcd_panel_vendor.h"
+
+#include "ili9488.h"
+
+static const char *TAG = "app_main";
+
+#define LCD_PIN_NUM_MOSI    13
+#define LCD_PIN_NUM_MISO    14
+#define LCD_PIN_NUM_SCLK    12
+#define LCD_PIN_NUM_CS      3
+#define LCD_PIN_NUM_DC      42
+#define LCD_PIN_NUM_RST     -1
+#define LCD_PIN_NUM_BL      46
+
+#define LCD_HOST            SPI2_HOST
+#define LCD_PIXEL_CLOCK_HZ  (40 * 1000 * 1000)
+#define LCD_H_RES           320
+#define LCD_V_RES           480
+
+#define LCD_CMD_BITS        8
+#define LCD_PARAM_BITS      8
 
 void app_main(void)
 {
-    printf("Esp-IDF version: %s\n", esp_get_idf_version());
+    ESP_LOGI(TAG, "Turn off LCD backlight...");
+    gpio_config_t bk_gpio_cong = {
+        .mode = GPIO_MODE_OUTPUT,
+        .pin_bit_mask = 1ULL << LCD_PIN_NUM_BL,
+    };
+    ESP_ERROR_CHECK(gpio_config(&bk_gpio_cong));
 
-    esp_chip_info_t chip_info;
-    esp_chip_info(&chip_info);
+    ESP_LOGI(TAG, "Initializing LCD bus...");
+    spi_bus_config_t buscfg = {
+        .sclk_io_num = LCD_PIN_NUM_SCLK,
+        .mosi_io_num = LCD_PIN_NUM_MOSI,
+        .miso_io_num = LCD_PIN_NUM_MISO,
+        .quadwp_io_num = GPIO_NUM_NC,
+        .quadhd_io_num = GPIO_NUM_NC,
+        .max_transfer_sz = 32768,
+    };
+    ESP_ERROR_CHECK(spi_bus_initialize(LCD_HOST, &buscfg, SPI_DMA_CH_AUTO));
 
-    printf("This is ESP32 chip with %d CPU cores\n", chip_info.cores);
+    ESP_LOGI(TAG, "Initializing panel IO...");
+    esp_lcd_panel_io_handle_t io_handle = NULL;
+    esp_lcd_panel_io_spi_config_t io_cfg = {
+        .dc_gpio_num = LCD_PIN_NUM_DC,
+        .cs_gpio_num = LCD_PIN_NUM_CS,
+        .pclk_hz = LCD_PIXEL_CLOCK_HZ,
+        .lcd_cmd_bits = LCD_CMD_BITS,
+        .lcd_param_bits = LCD_PARAM_BITS,
+        .spi_mode = 0,
+        .trans_queue_depth = 10,
+        .on_color_trans_done = NULL,
+        .user_ctx = NULL,
+        .flags = {
+            .dc_low_on_data = 0,
+            .octal_mode = 0,
+            .sio_mode = 0,
+            .lsb_first = 0,
+            .cs_high_active = 0,
+        },
+    };
+    ESP_ERROR_CHECK(esp_lcd_new_panel_io_spi((esp_lcd_spi_bus_handle_t)LCD_HOST, &io_cfg, &io_handle));
 
-    uint32_t flash_size = 0;
-    if (esp_flash_get_physical_size(NULL, &flash_size) == ESP_OK) {
-        printf("Physical flash size: %ld MB\n", flash_size / (1024 * 1024));
-    } else {
-        printf("Failed to get physical flash size!\n");
+    esp_lcd_panel_handle_t panel_handle = NULL;
+    esp_lcd_panel_dev_config_t panel_cfg = {
+        .reset_gpio_num = LCD_PIN_NUM_RST,
+        .rgb_ele_order = LCD_RGB_ELEMENT_ORDER_RGB,
+        .bits_per_pixel = 18,
+        .flags = {
+            .reset_active_high = 0,
+        },
+        .vendor_config = NULL
+    };
+    ESP_ERROR_CHECK(esp_lcd_new_panel_ili9488(io_handle, &panel_cfg, LCD_H_RES * 25, &panel_handle));
+
+    ESP_ERROR_CHECK(esp_lcd_panel_reset(panel_handle));
+    ESP_ERROR_CHECK(esp_lcd_panel_init(panel_handle));
+    ESP_ERROR_CHECK(esp_lcd_panel_invert_color(panel_handle, false));
+    ESP_ERROR_CHECK(esp_lcd_panel_swap_xy(panel_handle, false));
+    ESP_ERROR_CHECK(esp_lcd_panel_mirror(panel_handle, false, false));
+    ESP_ERROR_CHECK(esp_lcd_panel_set_gap(panel_handle, 0, 0));
+    ESP_ERROR_CHECK(esp_lcd_panel_disp_on_off(panel_handle, true));
+
+    gpio_set_direction(LCD_PIN_NUM_BL, GPIO_MODE_OUTPUT);
+    gpio_set_level(LCD_PIN_NUM_BL, 1);
+
+    uint16_t color = 0xF800; // Красный
+
+    size_t buffer_size = 50 * 50;
+    uint16_t *buffer = (uint16_t *)malloc(buffer_size * sizeof(uint16_t));
+    if (!buffer) {
+        printf("Error: Not enough memory for screen buffer\n");
+        return;
     }
-    printf("Free heap: %ld bytes\n", esp_get_free_heap_size());
+    for (size_t i = 0; i < buffer_size; i++) {
+        buffer[i] = color;
+    }
+
+    ESP_ERROR_CHECK(esp_lcd_panel_draw_bitmap(panel_handle, 0, 0, 50, 50, buffer));
+
+    free(buffer);
+
+    while (1) {
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
 }
